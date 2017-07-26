731fa0a82af05627d410bb7a5ebfc3d988786fef
Jeff Ripke <jripke74@gmail.com>
2017-07-26 11:34:45 -0700

    add currentWeather class

---
+++ WeatherApp/CurrentWeather.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by Jeff Ripke on 7/26/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipitationProbility: Double
+    let summary: String
+    let icon: String
+}
